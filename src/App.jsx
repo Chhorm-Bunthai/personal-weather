bbcfafae0f99526a18fdbf8bc85ac08a60a01630
bunthai <bunthaicambo89@gmail.com>
2023-09-06 13:54:55 +0700

    jj

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import './App.css'
 import Wrapper from './components/Wrapper'
-
 function App() {
   return (
     <>
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Time from './Time'
 import SearchBar from './SearchBar';
 import Location from './Location';
+import Pic1 from '../assets/1.svg'
+import Pic2 from '../assets/2.svg'
+import Pic3 from '../assets/3.svg'
+import Pic4 from '../assets/4.svg'
+import Pic5 from '../assets/sun.svg'
+import Pic6 from '../assets/rain with light.svg'
 
 function Wrapper() {
   return (
@@ -28,45 +34,46 @@ function Wrapper() {
                         <div className="idividual">
                             <p>TUE</p>
                             <div className="small-box">
-
+                                    <img src={Pic1} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
                         <div className="idividual">
-                            <p>TUE</p>
+                            <p>WED</p>
                             <div className="small-box">
-
+                                    <img src={Pic2} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
                         <div className="idividual">
-                            <p>TUE</p>
+                            <p>THUR</p>
                             <div className="small-box">
-
+                                    <img src={Pic3} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
                         <div className="idividual">
-                            <p>TUE</p>
+                            <p>FRI</p>
                             <div className="small-box">
-
+                                    <img src={Pic4} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
                         <div className="idividual">
-                            <p>TUE</p>
+                            <p>SAT</p>
                             <div className="small-box">
-
+                                    <img src={Pic5} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
                         <div className="idividual">
-                            <p>TUE</p>
+                            <p>SUN</p>
                             <div className="small-box">
-
+                                    <img src={Pic6} alt='tue'/>
                             </div>
                             <p>12 <sup>o</sup></p>
                         </div>
+                        
                     </div>
                     
 
